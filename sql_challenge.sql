d7a06773ea6c5de1286a2baa88f095b6f76027a5
Daniel Bernal <drbernal47@gmail.com>
2021-03-13 11:03:32 -0600

    Added code that creates tables to hold data

--- sql_challenge.sql
+++ sql_challenge.sql
@@ -1,5 +1,51 @@
+-- Drop tables if needed
+drop table if exists departments cascade;
+drop table if exists titles cascade;
+drop table if exists employees cascade;
+drop table if exists dept_emp cascade;
+drop table if exists dept_manager cascade;
+drop table if exists salaries cascade;
+
 -- Creating tables to hold data
-create table departments(
+create table if not exists departments(
 	dept_no varchar(32) not null primary key,
 	dept_name varchar(64) not null
-	);
+);
+
+create table if not exists titles(
+	title_id varchar(32) not null primary key,
+	title varchar(64) not null
+);
+
+create table if not exists employees(
+	emp_no int not null primary key,
+	emp_title_id varchar(64) not null,
+	birth_date date not null,
+	first_name varchar(64) not null,
+	last_name varchar(64) not null,
+	sex varchar(32),
+	hire_date date not null,
+	foreign key (emp_title_id) references titles(title_id)
+);
+	
+create table if not exists dept_emp(
+	emp_no int not null,
+	dept_no varchar(32) not null,
+	primary key (emp_no, dept_no),
+	foreign key (dept_no) references departments(dept_no),
+	foreign key (emp_no) references employees(emp_no)
+);
+
+create table if not exists dept_manager(
+	dept_no varchar(32) not null,
+	emp_no int not null,
+	primary key (dept_no, emp_no),
+	foreign key (dept_no) references departments(dept_no),
+	foreign key (emp_no) references employees(emp_no)
+);
+
+create table if not exists salaries(
+	emp_no int not null primary key,
+	salary int not null,
+	foreign key (emp_no) references employees(emp_no)
+);
